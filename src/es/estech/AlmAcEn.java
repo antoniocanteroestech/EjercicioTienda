5ddaa98b703ca7bba140f2fcd67f48a535126710
Antonio Cantero Ruiz <acanteroruiz.dev@gmail.com>
2018-12-01 09:52:56 +0100

    first commit

---
+++ src/es/estech/AlmAcEn.java
@@ -0,0 +1,10 @@
+package es.estech;
+import java.util.ArrayList;
+
+public class AlmAcEn {
+    public ArrayList<Producto> productos;
+
+    public AlmAcEn() {
+        productos = new ArrayList<>();
+    }
+}
